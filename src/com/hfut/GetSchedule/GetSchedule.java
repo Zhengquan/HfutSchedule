41c0ad87b41899273a955eafc0eb40ef86503167
Zhengquan <yangzhengquan@gmail.com>
2011-03-02 18:34:18 +0800

    完成了按钮的事件响应，实现视图的切换，并改观了部分界面

--- src/com/hfut/GetSchedule/GetSchedule.java
+++ src/com/hfut/GetSchedule/GetSchedule.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
 import com.util.GetSchedule.CookiesManager;
 
 public class GetSchedule extends Activity {
@@ -16,7 +14,6 @@ public class GetSchedule extends Activity {
 	private Button mButton2;
 	private EditText mEditText1;
 	private EditText mEditText2;
-	private TextView mTextView1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +22,6 @@ public class GetSchedule extends Activity {
         mButton2 = (Button)findViewById(R.id.button2);
         mEditText1 = (EditText)findViewById(R.id.edittext1);
         mEditText2 = (EditText)findViewById(R.id.edittext2);
-        mTextView1 = (TextView)findViewById(R.id.textview0);
-        
         initialClickedListener();
 	}
     private void initialClickedListener() {
@@ -42,18 +37,20 @@ public class GetSchedule extends Activity {
 				try {
 					boolean login_success = cookiesManager.GenerateCookies();
 					if(login_success){
-//						mTextView1.setText("登陆成功");
 						Intent intent  = new Intent();
 						intent.setClass(GetSchedule.this, ScheduleView.class);
 						Bundle bundle = new Bundle();
-						bundle.putString("user",mEditText1.getText().toString());
-						bundle.putString("password",mEditText2.getText().toString());
+						String user = mEditText1.getText().toString();
+						String password = mEditText2.getText().toString();
+						bundle.putString("user",user);
+						bundle.putString("password",password);
 						intent.putExtras(bundle);
 						startActivity(intent);
+					}else{
+						//登陆失败
 					}
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
-					mTextView1.setText("登陆失败");
 					e.printStackTrace();
 				}
 			}
--- src/com/hfut/GetSchedule/ScheduleView.java
+++ src/com/hfut/GetSchedule/ScheduleView.java
@@ -1,10 +1,16 @@
 package com.hfut.GetSchedule;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
+import java.util.Locale;
+import java.text.DateFormat;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.util.GetSchedule.ParseHtml;
@@ -12,19 +18,36 @@ import com.util.GetSchedule.CookiesManager;
 import com.util.GetSchedule.ScheduleManager;
 
 public class ScheduleView extends Activity {
+	private TextView textViewDate ;
+	private Handler handler;
+	private TextView textViewArraya;
+	private TextView textViewArrayb;
+	private TextView textViewArrayc;
+	private TextView textViewArrayd;
+	private TextView textViewArraye;
+	
+	private Button mButton_next;
+	private Button mButton_former;
+	private ArrayList<ArrayList<String>> scheduleData;
+	
+	private int next_count;
+	private static final int UPDATE_DATE_TEXTVIEW = 0x111;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.schedule);
 		//初始化TextView控件
+		createHandler();
+		textViewArraya = (TextView)findViewById(R.id.schedule1);//0,1
+		textViewArrayb = (TextView)findViewById(R.id.schedule2);//2,3
+		textViewArrayc = (TextView)findViewById(R.id.schedule4);//4,5
+		textViewArrayd = (TextView)findViewById(R.id.schedule5);//6,7
+		textViewDate = (TextView)findViewById(R.id.textview3);
+		textViewArraye = (TextView)findViewById(R.id.schedule3);
 		
-		TextView textViewArraya = (TextView)findViewById(R.id.schedule1);//0,1
-		TextView textViewArrayb = (TextView)findViewById(R.id.schedule2);//2,3
-		TextView textViewArrayc = (TextView)findViewById(R.id.schedule4);//4,5
-		TextView textViewArrayd = (TextView)findViewById(R.id.schedule5);//6,7
-		
-		
+		mButton_former =(Button)findViewById(R.id.button_former);
+		mButton_next = (Button)findViewById(R.id.button_Next);
 		Bundle bundle = new Bundle();
 		bundle = this.getIntent().getExtras();
 		CookiesManager cookiesManager = new CookiesManager(
@@ -35,18 +58,79 @@ public class ScheduleView extends Activity {
 		try {
 			cookiesManager.GenerateCookies();
 			ParseHtml parseHtml = new ParseHtml(cookiesManager.defaultHttpClient);
-			ArrayList<ArrayList<String>> scheduleData = parseHtml.getScheduleThisTerm();
-			parseHtml.getUserInformation();
-			ArrayList<String> schedule = ScheduleManager.getDaysSchedule(scheduleData, 1);
-			
-			textViewArraya.setText(schedule.get(0));
-			textViewArrayb.setText(schedule.get(2));
-			textViewArrayc.setText(schedule.get(4));
-			textViewArrayd.setText(schedule.get(6));
-			
+			scheduleData = parseHtml.getScheduleThisTerm();
+			//parseHtml.getUserInformation();
+			ArrayList<String> schedule = ScheduleManager.getDaysSchedule(scheduleData, 0);
+			DateManagerThread dateManagerThread = new DateManagerThread();
+			setTextScheduleView(schedule);
+			dateManagerThread.start();
+			initButtonListener();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	void initButtonListener(){
+		next_count = 0;
+		textViewArraye.setText("  中午  ");
+		mButton_next.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				next_count++;
+				ArrayList<String> schedule = ScheduleManager.getDaysSchedule(scheduleData, next_count);
+				setTextScheduleView(schedule);
+			}
+		});
+		mButton_former.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				if(next_count == 0){
+					next_count = 7;
+				}
+				ArrayList<String> schedule = ScheduleManager.getDaysSchedule(scheduleData, --next_count);
+				setTextScheduleView(schedule);
+			}
+		});
+	}
+	void setTextScheduleView(ArrayList<String> schedule){
+		textViewArraya.setText(schedule.get(0));
+		textViewArrayb.setText(schedule.get(2));
+		textViewArrayc.setText(schedule.get(4));
+		textViewArrayd.setText(schedule.get(6));
+	}
+	private void createHandler() {
+		// TODO Auto-generated method stub
+		handler = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				// TODO Auto-generated method stub
+				super.handleMessage(msg);
+				switch(msg.what){
+				case UPDATE_DATE_TEXTVIEW:
+					Date date = new Date();
+					DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.MEDIUM,DateFormat.MEDIUM,Locale.CHINA);
+					String formattedStr = dateFormat.format(date);
+					textViewDate.setText("Today "+formattedStr);
+				break;
+				}
+			}
+		};
+	}
+	public class DateManagerThread extends Thread {
+		@Override
+		public void run() {
+			while(true){
+				try {
+					Message msg = new Message();
+					msg.what = UPDATE_DATE_TEXTVIEW;
+					ScheduleView.this.handler.sendMessage(msg);
+					sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
